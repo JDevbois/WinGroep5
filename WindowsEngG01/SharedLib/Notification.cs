c23970b125d886afa76d784ef9fe562bdd49b393
Joren Debois <Joren.Debois.y9782@student.hogent.be>
2019-08-15 21:20:58 +0200

    [domain] add notification datatype

---
+++ WindowsEngG01/SharedLib/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib
+{
+    public class Notification
+    {
+        public String Id { get; set; }
+        public String CompanyId { get; set; }
+        public String UserId { get; set; }
+        public String PromotionId { get; set; }
+        public int PromotionTypeIdentifier { get; set; }
+
+        public Notification()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
