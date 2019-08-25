f030962d60379a5b8ad23c78bd758a7867723d2c
Joren Debois <Joren.Debois.y9782@student.hogent.be>
2019-08-25 18:02:41 +0200

    ss

---
+++ WindowsEngG01/BackendAPI/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendAPI.Models
+{
+    public class Company : SharedLib.Company
+    {
+        
+    }
+}
